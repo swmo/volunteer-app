3acccaaa3b2050836379e815d2ae0a98da5781b7
Moses Tschanz <moses.tschanz@gmail.com>
2019-03-03 15:27:52 +0100

    testing

---
+++ symfony/src/Controller/MissionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MissionController extends AbstractController
+{
+
+    /**
+     * @Route("/mission/create", name="mission_create")
+     */
+    public function create()
+    {
+        return $this->render('mission/create.html.twig', [
+            'controller_name' => 'Create a new Mission',
+        ]);
+    }
+
+}
