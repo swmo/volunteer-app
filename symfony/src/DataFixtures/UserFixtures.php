c4d197bde8066507d88fe5a50b63552f5e164e01
Moses Tschanz <moses.tschanz@gmail.com>
2019-04-27 15:48:16 +0200

    User Class

---
+++ symfony/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('personal@burdorfer-stadtlauf.ch');
+        $user->setRoles(array('ROLE_ADMIN'));
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'demo'
+        ));
+
+        $manager->persist($user);
+
+       $manager->flush();
+    }
+
+}
