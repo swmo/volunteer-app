fd45c30924b7f732be5be2e074fe2b56aa0675f1
Moses Tschanz <moses.tschanz@gmail.com>
2020-02-12 18:44:37 +0100

    fixtures: first Enrollment Fixture

--- src/DataFixtures/EnrollmentFixtures.php
+++ src/DataFixtures/EnrollmentFixtures.php
@@ -2,16 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Enrollment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class EnrollmentFixtures extends Fixture
+class EnrollmentFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $enrollment = new Enrollment();
+        $enrollment
+        ->setBirthday(new \Datetime())
+        ->setCity('Bern')
+        ->setComment('Bitte kurz anrufen, bin verunsicher ob dieser Einsatz für mich ok ist.')
+        ->setConfirmToken('asjfdsjfjsalfjasl')
+        ->setEmail('moses.tschanz@gmail.com')
+        ->setFirstname('Moses')
+        ->setLastname('Tschanz')
+        ->setMissionChoice01($this->getReference('Mission_Streckenposten'))
+        ->setMobile('088 999 999 99')
+        ->setZip(3600)
+        ->setStreet('Teststrasse 10')
+        ->setHasTshirt(true);
+        $manager->persist($enrollment);
+        
+    
         $manager->flush();
     }
 
--- src/DataFixtures/UserOrganisationFixtures.php
+++ src/DataFixtures/UserOrganisationFixtures.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\UserOrganisation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class UserOrganisationFixtures extends Fixture
+class UserOrganisationFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
