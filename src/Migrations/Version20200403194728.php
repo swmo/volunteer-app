5245d68779e13827b20921b69964bf23d57227d9
root <root@ip-172-26-8-190.eu-central-1.compute.internal>
2020-04-03 20:13:22 +0000

    v

---
+++ src/Migrations/Version20200403194728.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200403194728 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE organisation ADD public BOOLEAN DEFAULT \'false\' NOT NULL');
+         $this->addSql('ALTER TABLE enrollment ADD project_id INT NULL');
+$this->addSql('UPDATE enrollment SET project_id=1');
+ $this->addSql('ALTER TABLE enrollment ALTER project_id SET NOT NULL');
+        $this->addSql('ALTER TABLE enrollment ADD CONSTRAINT FK_DBDCD7E1166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_DBDCD7E1166D1F9C ON enrollment (project_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE organisation DROP public');
+        $this->addSql('ALTER TABLE enrollment DROP CONSTRAINT FK_DBDCD7E1166D1F9C');
+        $this->addSql('DROP INDEX IDX_DBDCD7E1166D1F9C');
+        $this->addSql('ALTER TABLE enrollment DROP project_id');
+    }
+}
