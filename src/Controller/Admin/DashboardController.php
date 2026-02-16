fb9e6406fefe63757bcd5b2674fa6bb9b25e1e7d
Moses <moses@macbook-pro-von-work.home>
2026-02-16 21:45:37 +0100

    Add active-project enrollment stats to dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Project;
 use App\Entity\Person;
 use App\Utils\MonologDbHandler;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,17 +15,56 @@ class DashboardController extends AbstractController
 
     public function dashboard(MonologDbHandler $db, EntityManagerInterface $em)
     {
-       
-        
         $persons = $em->getRepository(Person::class)->findAll();
+        $selectedOrganisation = $this->getUser()?->getSelectedOrganisation();
+        $projects = [];
+        $projectStats = [];
+        $totalOpenEnrollments = 0;
+        $totalEnrolledPersons = 0;
 
-        //Bu$enrollments = $this->
+        if ($selectedOrganisation !== null) {
+            $projects = $em->getRepository(Project::class)->findBy(
+                [
+                    'organisation' => $selectedOrganisation,
+                    'isEnabled' => true,
+                ],
+                ['name' => 'ASC']
+            );
+        }
+
+        foreach ($projects as $project) {
+            $requiredSlots = 0;
+            $filledSlots = 0;
+
+            foreach ($project->getMissions() as $mission) {
+                if (!$mission->getIsEnabled()) {
+                    continue;
+                }
+
+                $requiredSlots += (int) $mission->getRequiredVolunteers();
+                $filledSlots += (int) $mission->countEnrolledVolunteers();
+            }
+
+            $enrolledPersons = $project->getEnrollments()->count();
+            $openEnrollments = max(0, $requiredSlots - $filledSlots);
+
+            $totalEnrolledPersons += $enrolledPersons;
+            $totalOpenEnrollments += $openEnrollments;
+
+            $projectStats[] = [
+                'project' => $project,
+                'enrolledPersons' => $enrolledPersons,
+                'openEnrollments' => $openEnrollments,
+            ];
+        }
 
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'Test',
-            'persons' => $persons
+            'persons' => $persons,
+            'projectStats' => $projectStats,
+            'totalOpenEnrollments' => $totalOpenEnrollments,
+            'totalEnrolledPersons' => $totalEnrolledPersons,
         ]);
     }
 
 }
-
