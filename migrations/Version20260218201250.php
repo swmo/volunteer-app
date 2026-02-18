e398505717b2fe11dde9ecde88fca335e0b3c563
Moses <moses@zmku169dignb13.home>
2026-02-18 22:31:42 +0100

    remove old migrations and update postgressql

--- migrations/Version20260218201250.php
+++ migrations/Version20260218201250.php
@@ -10,24 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190305210829 extends AbstractMigration
+final class Version20260218201250 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
 
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
         $this->addSql('CREATE SCHEMA public');
     }
 }
